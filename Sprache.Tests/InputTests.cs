f7e419c6c68b4896f17141f1a1d487430909b0dc
mail.alex.lyman%40gmail.com <mail.alex.lyman%40gmail.com@localhost>
2011-01-22 17:10:22 -0800

    Adds line counting to inputs

--- Sprache.Tests/InputTests.cs
+++ Sprache.Tests/InputTests.cs
@@ -66,5 +66,75 @@ namespace Sprache.Tests
             var i = new Input("abc");
             Assert.AreEqual(0, i.Position);
         }
+
+        [Test]
+        public void AdvancingInput_IncreasesColumnNumber()
+        {
+            var i = new Input("abc", 1);
+            var j = i.Advance();
+            Assert.AreEqual(2, j.Column);
+        }
+        [Test]
+        public void AdvancingInputAtEOL_IncreasesLineNumber()
+        {
+            var i = new Input("\nabc");
+            var j = i.Advance();
+            Assert.AreEqual(2, j.Line);
+        }
+
+        [Test]
+        public void AdvancingInputAtEOL_ResetsColumnNumber()
+        {
+            var i = new Input("\nabc");
+            var j = i.Advance();
+            Assert.AreEqual(2, j.Line);
+            Assert.AreEqual(1, j.Column);
+        }
+
+        [Test]
+        public void LineCountingSmokeTest()
+        {
+            var i = new Input("abc\ndef");
+            Assert.AreEqual(0, i.Position);
+            Assert.AreEqual(1, i.Line);
+            Assert.AreEqual(1, i.Column);
+
+            i = i.AdvanceAssert((a, b) =>
+            {
+                Assert.AreEqual(1, b.Position);
+                Assert.AreEqual(1, b.Line);
+                Assert.AreEqual(2, b.Column);
+            });
+            i = i.AdvanceAssert((a, b) =>
+            {
+                Assert.AreEqual(2, b.Position);
+                Assert.AreEqual(1, b.Line);
+                Assert.AreEqual(3, b.Column);
+            });
+            i = i.AdvanceAssert((a, b) =>
+            {
+                Assert.AreEqual(3, b.Position);
+                Assert.AreEqual(1, b.Line);
+                Assert.AreEqual(4, b.Column);
+            });
+            i = i.AdvanceAssert((a, b) =>
+            {
+                Assert.AreEqual(4, b.Position);
+                Assert.AreEqual(2, b.Line);
+                Assert.AreEqual(1, b.Column);
+            });
+            i = i.AdvanceAssert((a, b) =>
+            {
+                Assert.AreEqual(5, b.Position);
+                Assert.AreEqual(2, b.Line);
+                Assert.AreEqual(2, b.Column);
+            });
+            i = i.AdvanceAssert((a, b) =>
+            {
+                Assert.AreEqual(6, b.Position);
+                Assert.AreEqual(2, b.Line);
+                Assert.AreEqual(3, b.Column);
+            });
+        }
     }
 }
